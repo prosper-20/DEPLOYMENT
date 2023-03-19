709d00111aefbada3121922112dfb87d3a5eb08e
prosper <edwardprosper001@gmail.com>
2023-03-19 16:17:45 +0100

    upload

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -120,6 +120,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Default primary key field type
